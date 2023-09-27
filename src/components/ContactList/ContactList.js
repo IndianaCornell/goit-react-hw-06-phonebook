5e60c2fd81cb8f7354c4cbe154c93941e24a59c0
Serj Pyshkin <IndianaCornell@gmail.com>
2023-09-27 10:51:25 +0300

    Redux Toolkit
    Redux Toolkit

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,35 @@
+import { deleteContact } from 'redux/contactsSlice';
+import { ItemsList, StyledItem, DeleteButton } from './ContactList.styled';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+
+const localStorageKey = 'quiz-contacts';
+
+export const ContactList = () => {
+  const dispatch = useDispatch();
+
+  const contacts = useSelector(state => state.contacts);
+
+  const filter = useSelector(state => state.filter);
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(contacts));
+  }, [contacts]);
+
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <ItemsList>
+      {visibleContacts.map(contact => (
+        <StyledItem key={contact.id}>
+          {contact.name}: {contact.number}
+          <DeleteButton onClick={() => dispatch(deleteContact(contact.id))}>
+            Delete
+          </DeleteButton>
+        </StyledItem>
+      ))}
+    </ItemsList>
+  );
+};
---
+++ src/redux/filterSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initFilter = '';
+
+const slice = createSlice({
+  name: 'filter',
+  initialState: initFilter,
+  reducers: {
+    filteredContacts(state, action) {
+      return action.payload;
+    },
+  },
+});
+
+export const { filteredContacts } = slice.actions;
+export const filterReducer = slice.reducer;
+
+// export const filteredContacts = createAction('filter/changeFilter');
+
+// export const filterReducer = createReducer('', builder =>
+//   builder.addCase(filteredContacts, (state, action) => action.payload)
+// );
